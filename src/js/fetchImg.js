4604326883e2b14d8d4d42707ecd9d9b7c986f28
Yevhen Cherednyk <yevgen.cherednyk@gmail.com>
2022-11-13 22:08:28 +0200

    add final code

--- src/js/fetchImg.js
+++ src/js/fetchImg.js
@@ -1,5 +1,51 @@
+import axios from 'axios';
+
 const BASE_URL = 'https://pixabay.com/api';
 const API_KEY = '31186773-8484a0bc913959b467e4295b5';
+
+// with use fetch syntax
+
+// export default class PhotoApiService {
+//   constructor() {
+//     this.name = '';
+//     this.PARE_PAGE = 40;
+//     this.page = 1;
+//   }
+
+//   fetchImg() {
+//     const url = `${BASE_URL}/?key=${API_KEY}&q=${this.name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${this.PARE_PAGE}&page=${this.page}`;
+
+//     return fetch(url)
+//       .then(response => {
+//         if (!response.ok) {
+//           throw new Error(response.status);
+//         }
+//         return response.json();
+//       })
+//       .then(data => {
+//         this.incrementPage();
+//         return data;
+//       });
+//   }
+
+//   incrementPage() {
+//     this.page += 1;
+//   }
+
+//   resetPage() {
+//     this.page = 1;
+//   }
+
+//   get query() {
+//     return this.name;
+//   }
+
+//   set query(newName) {
+//     this.name = newName;
+//   }
+// }
+
+// with use library Axios and async/await syntaxis
 
 export default class PhotoApiService {
   constructor() {
@@ -8,20 +54,16 @@ export default class PhotoApiService {
     this.page = 1;
   }
 
-  fetchImg() {
-    const url = `${BASE_URL}/?key=${API_KEY}&q=${this.name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${this.PARE_PAGE}&page=${this.page}`;
-
-    return fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
-        return response.json();
-      })
-      .then(data => {
-        this.page += 1;
-        return data.hits;
-      });
+  async fetchImg() {
+    try {
+      const url = `${BASE_URL}/?key=${API_KEY}&q=${this.name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${this.PARE_PAGE}&page=${this.page}`;
+      const response = await axios.get(url);
+      const data = await response.data;
+      this.incrementPage();
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   incrementPage() {
--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,31 @@
 import '../css/styles.css';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import PhotoApiService from './fetchImg';
 import createCardMarkup from './cardMarkup';
+import LoadMoreBtn from './loadMoreBtn';
 
 const refs = {
   formRef: document.querySelector('#search-form'),
   galleryRef: document.querySelector('.gallery'),
-  loadMoreBtn: document.querySelector('.load-more'),
+  // loadMoreBtn: document.querySelector('.load-more'),
 };
 
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
 const photoApiService = new PhotoApiService();
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '.load-more',
+  hidden: true,
+});
 
 refs.formRef.addEventListener('submit', onSearchBtnClick);
-refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
+loadMoreBtn.refs.button.addEventListener('click', onLoadMoreBtnClick);
+// refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
 
 function onSearchBtnClick(evt) {
   evt.preventDefault();
@@ -22,27 +35,68 @@ function onSearchBtnClick(evt) {
 
   if (!photoApiService.query) {
     clearMarup();
-    return;
+    loadMoreBtn.hide();
+    return Notiflix.Notify.warning('Please enter a valid value');
   }
+
+  loadMoreBtn.show();
+  loadMoreBtn.disable();
 
   photoApiService
     .fetchImg()
-    .then(hits => {
+    .then(({ totalHits, hits }) => {
       clearMarup();
+
+      if (hits.length === 0) {
+        loadMoreBtn.hide();
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
+      }
       appendCardMarkup(hits);
+      loadMoreBtn.enable();
     })
     .catch(error => console.log(error));
 }
 
 function onLoadMoreBtnClick(evt) {
+  loadMoreBtn.disable();
+
   photoApiService
     .fetchImg()
-    .then(appendCardMarkup)
+    .then(({ totalHits, hits }) => {
+      const totalPages = Math.ceil(totalHits / hits.length);
+      const currentPage = photoApiService.page - 1;
+      if (totalPages <= currentPage) {
+        Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+        loadMoreBtn.hide();
+      }
+      appendCardMarkup(hits);
+      loadMoreBtn.enable();
+
+      // smooth scroll start
+
+      const { height: cardHeight } = document
+        .querySelector('.gallery')
+        .firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
+
+      // smooth scroll end
+    })
     .catch(error => console.log(error));
 }
 
 function appendCardMarkup(hits) {
   refs.galleryRef.insertAdjacentHTML('beforeend', createCardMarkup(hits));
+  gallery.refresh();
 }
 
 function clearMarup() {
